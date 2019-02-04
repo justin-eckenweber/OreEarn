a6671d263d5a684fd26b0616588fcbf1e27bc536
SchdowNVIDIA <39440218+SchdowNVIDIA@users.noreply.github.com>
2019-02-04 22:15:04 +0100

    Big Performance Boost

--- OreEarn/src/OreEarn/Main.php
+++ OreEarn/src/OreEarn/Main.php
@@ -50,18 +50,8 @@ class Main extends PluginBase implements Listener
             $lucklevel = 1;
         }
 
-        $stoneEarn = $this->getConfig()->getNested("earnings.stoneEarn") * $lucklevel;
-        $coalEarn = $this->getConfig()->getNested("earnings.coalEarn") * $lucklevel;
-        $redstoneEarn = $this->getConfig()->getNested("earnings.redstoneEarn") * $lucklevel;
-        $lapisEarn = $this->getConfig()->getNested("earnings.lapisEarn") * $lucklevel;
-        $diamondEarn = $this->getConfig()->getNested("earnings.diamondEarn") * $lucklevel;
-        $emeraldEarn = $this->getConfig()->getNested("earnings.emeraldEarn") * $lucklevel;
-        $ironEarn = $this->getConfig()->getNested("earnings.ironEarn") * $lucklevel;
-        $goldEarn = $this->getConfig()->getNested("earnings.goldEarn") * $lucklevel;
-
-        $luckdrop = $lucklevel;
-
         if($id == Block::STONE) {
+            $stoneEarn = $this->getConfig()->getNested("earnings.stoneEarn") * $lucklevel;
             $extraStone = rand(1, 100);
             if($extraStone > 98) {
                 $event->setDrops(array(Item::get(4), Item::get(371, 0, rand(0, 1))));
@@ -71,38 +61,45 @@ class Main extends PluginBase implements Listener
             $this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->addMoney($name, $stoneEarn);
         }
         if($id == Block::COAL_ORE) {
-            $event->setDrops(array(Item::get(263, 0 , rand(1, $luckdrop))));
+            $coalEarn = $this->getConfig()->getNested("earnings.coalEarn") * $lucklevel;
+            $event->setDrops(array(Item::get(263, 0 , rand(1, $lucklevel))));
             $this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->addMoney($name, $coalEarn);
             $player->sendPopup("Du hast soeben §6" . $coalEarn . "$ §ffür das abbauen von §6Coal Ore §ferhalten.");
         }
         if($id == Block::REDSTONE_ORE || $id == Block::GLOWING_REDSTONE_ORE) {
-            $event->setDrops(array(Item::get(331, 0 , rand(1, $luckdrop))));
+            $redstoneEarn = $this->getConfig()->getNested("earnings.redstoneEarn") * $lucklevel;
+            $event->setDrops(array(Item::get(331, 0 , rand(1, $lucklevel))));
             $this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->addMoney($name, $redstoneEarn);
             $player->sendPopup("Du hast soeben §6" . $redstoneEarn . "$ §ffür das abbauen von §6Redstone Ore §ferhalten.");
         }
         if($id == Block::LAPIS_ORE) {
-            $event->setDrops(array(Item::get(351, 4 , rand(1, $luckdrop))));
+            $lapisEarn = $this->getConfig()->getNested("earnings.lapisEarn") * $lucklevel;
+            $event->setDrops(array(Item::get(351, 4 , rand(1, $lucklevel))));
             $this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->addMoney($name, $lapisEarn);
             $player->sendPopup("Du hast soeben §6" . $lapisEarn . "$ §ffür das abbauen von §6Lapis Ore §ferhalten.");
         }
         if($id == Block::DIAMOND_ORE) {
-            $event->setDrops(array(Item::get(264, 0 , rand(1, $luckdrop))));
+            $diamondEarn = $this->getConfig()->getNested("earnings.diamondEarn") * $lucklevel;
+            $event->setDrops(array(Item::get(264, 0 , rand(1, $lucklevel))));
             $this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->addMoney($name, $diamondEarn);
             $player->sendPopup("Du hast soeben §6" . $diamondEarn . "$ §ffür das abbauen von §6Diamond Ore §ferhalten.");
         }
         if($id == Block::EMERALD_ORE) {
+            $emeraldEarn = $this->getConfig()->getNested("earnings.emeraldEarn") * $lucklevel;
             $event->setDrops(array(Item::get(388, 0 , rand(1, 2))));
             $this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->addMoney($name, $emeraldEarn);
             $player->sendPopup("Du hast soeben §6" . $emeraldEarn . "$ §ffür das abbauen von §6Emerald Ore §ferhalten.");
         }
         if($id == Block::IRON_ORE) {
-            $event->setDrops(array(Item::get(265), Item::get(452, 0, rand(0, $luckdrop))));
+            $ironEarn = $this->getConfig()->getNested("earnings.ironEarn") * $lucklevel;
+            $event->setDrops(array(Item::get(265), Item::get(452, 0, rand(0, $lucklevel))));
             $event->setXpDropAmount(2.75);
             $this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->addMoney($name, $ironEarn);
             $player->sendPopup("Du hast soeben §6" . $ironEarn . "$ §ffür das abbauen von §6Iron Ore §ferhalten.");
         }
         if($id == Block::GOLD_ORE) {
-            $event->setDrops(array(Item::get(266), Item::get(371, 0, rand(0, $luckdrop))));
+            $goldEarn = $this->getConfig()->getNested("earnings.goldEarn") * $lucklevel;
+            $event->setDrops(array(Item::get(266), Item::get(371, 0, rand(0, $lucklevel))));
             $event->setXpDropAmount(5);
             $this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->addMoney($name, $goldEarn);
             $player->sendPopup("Du hast soeben §6" . $goldEarn . "$ §ffür das abbauen von §6Gold Ore §ferhalten.");
